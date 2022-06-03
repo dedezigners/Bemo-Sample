8fb6783f402789adeec03ef936422b1d60fd32ef
Atif Ibrahim <dedezigners@gmail.com>
2022-06-03 20:39:03 +0500

    update migration

--- database/migrations/2022_06_02_205210_create_cards_table.php
+++ database/migrations/2022_06_02_205210_create_cards_table.php
@@ -18,7 +18,7 @@ class CreateCardsTable extends Migration
 
             $table->string('title');
             $table->text('desc')->nullable();
-            $table->integer('position')->default(0);
+            $table->integer('position');
             
             $table->foreignId('column_id')->constrained();
             $table->softDeletes();
